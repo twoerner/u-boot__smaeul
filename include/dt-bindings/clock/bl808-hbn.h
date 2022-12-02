d564ac83580a136cec414758082ffd09527afe58
Samuel Holland <samuel@sholland.org>
2022-12-01 22:16:37 -0600

    clk: Add BL808 clock driver
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/dt-bindings/clock/bl808-hbn.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0+ or MIT) */
+
+#ifndef _DT_BINDINGS_CLOCK_BL808_HBN_H_
+#define _DT_BINDINGS_CLOCK_BL808_HBN_H_
+
+#define CLK_RC32K		0
+#define CLK_RC32M		1
+#define CLK_XCLK		2
+#define CLK_HBN_ROOT		3
+#define CLK_HBN_UART		4
+#define CLK_F32K		5
+#define CLK_HBN_UART_SEL2	6
+
+#endif /* _DT_BINDINGS_CLOCK_BL808_HBN_H_ */
